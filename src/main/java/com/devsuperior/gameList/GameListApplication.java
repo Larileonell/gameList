ce706fc99d5ca51450d289f052c4e9f439c840aa
Larissa leonel <larissaleonel98@gmail.com>
2025-01-23 12:06:44 -0300

    Domain Model

--- src/main/java/com/devsuperior/gameList/GameListApplication.java
+++ src/main/java/com/devsuperior/gameList/GameListApplication.java
@@ -2,12 +2,12 @@ package com.devsuperior.gameList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.devsuperior.gameList")
 public class GameListApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GameListApplication.class, args);
 	}
-
 }
--- src/main/java/com/devsuperior/gameList/controllers/GameController.java
+++ src/main/java/com/devsuperior/gameList/controllers/GameController.java
@@ -4,6 +4,7 @@ import com.devsuperior.gameList.dto.GameMinDTO;
 import com.devsuperior.gameList.entities.Game;
 import com.devsuperior.gameList.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +13,13 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/games")
 public class GameController {
+
     @Autowired
     private GameService gameService;
 
+    @GetMapping
     public List<GameMinDTO> findAll() {
         List<GameMinDTO> result = gameService.findAll();
         return result;
     }
-    }
+}
